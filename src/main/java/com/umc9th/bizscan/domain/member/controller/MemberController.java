1dfbef848fb5196b5133203fadf29dea33233158
shinchaerin79 <166346480+shinchaerin79@users.noreply.github.com>
2026-01-16 21:08:38 +0900

    :wrench: Settings: GitHub Actions 기반 CI/CD 및 Docker 배포 환경 설정
    * :wrench: Settings: GitHub Actions 기반 CI/CD 및 Docker 배포 환경 설정
    
    * :wrench: Settings: Spotless 코드 포맷터 설정 추가
    
    * :wrench: Settings: Spotless 포맷 적용

--- src/main/java/com/umc9th/bizscan/domain/member/controller/MemberController.java
+++ src/main/java/com/umc9th/bizscan/domain/member/controller/MemberController.java
@@ -1,6 +1,5 @@
 package com.umc9th.bizscan.domain.member.controller;
 
-
 import com.umc9th.bizscan.global.apiPayload.code.ErrorCode;
 import com.umc9th.bizscan.global.config.swagger.ApiErrorCodeExamples;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/member")
 public class MemberController {
-    @GetMapping("/test")
-    @ApiErrorCodeExamples({ErrorCode.BAD_REQUEST, ErrorCode.FORBIDDEN, ErrorCode.NOT_FOUND})
-    public String test() {
-        return "Hello, World!";
-    }
+  @GetMapping("/test")
+  @ApiErrorCodeExamples({ErrorCode.BAD_REQUEST, ErrorCode.FORBIDDEN, ErrorCode.NOT_FOUND})
+  public String test() {
+    return "Hello, World!";
+  }
 }
--- src/main/java/com/umc9th/bizscan/global/config/swagger/ExampleHolder.java
+++ src/main/java/com/umc9th/bizscan/global/config/swagger/ExampleHolder.java
@@ -4,9 +4,4 @@ import io.swagger.v3.oas.models.examples.Example;
 import lombok.Builder;
 
 @Builder
-public record ExampleHolder(
-        Example holder,
-        String name,
-        int status,
-        String code
-) { }
+public record ExampleHolder(Example holder, String name, int status, String code) {}
