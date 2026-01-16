da54ffd1f204cc381124bd8610154562661c8844
shinchaerin79 <shinchaerin79@gmail.com>
2026-01-16 21:01:20 +0900

    :wrench: Settings: Spotless 포맷 적용

--- src/main/java/com/umc9th/bizscan/global/health/HealthController.java
+++ src/main/java/com/umc9th/bizscan/global/health/HealthController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
-    @GetMapping("/health")
-    public String health() {
-        return "Hello, World!";
-    }
+  @GetMapping("/health")
+  public String health() {
+    return "Hello, World!";
+  }
 }
