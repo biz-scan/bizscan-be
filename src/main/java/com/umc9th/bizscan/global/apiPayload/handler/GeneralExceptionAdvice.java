697150f19db0625b0c41323bfe04727b06d951e5
kjh015 <khj843@naver.com>
2026-01-30 09:10:59 +0900

    ♻️ Refactor: MethodArgumentNotValidException 전역 예외 추가

--- src/main/java/com/umc9th/bizscan/global/apiPayload/handler/GeneralExceptionAdvice.java
+++ src/main/java/com/umc9th/bizscan/global/apiPayload/handler/GeneralExceptionAdvice.java
@@ -8,8 +8,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 @RequiredArgsConstructor
@@ -22,6 +26,22 @@ public class GeneralExceptionAdvice {
     return ResponseEntity.status(ex.getCode().getStatus())
         .body(ApiResponse.onFailure(ex.getCode(), null));
   }
+
+    // 컨트롤러 메서드에서 @Valid 어노테이션을 사용하여 DTO의 유효성 검사를 수행
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ApiResponse<Map<String, String>>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        // 검사에 실패한 필드와 그에 대한 메시지를 저장하는 Map
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+
+        ErrorCode code = ErrorCode.INVALID_TYPE_VALUE;
+        ApiResponse<Map<String, String>> errorResponse = ApiResponse.onFailure(code, errors);
+
+        // 에러 코드, 메시지와 함께 errors를 반환
+        return ResponseEntity.status(code.getStatus()).body(errorResponse);
+    }
 
   // 그 외의 정의되지 않은 모든 예외 처리
   @ExceptionHandler(Exception.class)
