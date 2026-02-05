71d830b2960022cda984f0c5ff7f0f290a41e651
shinchaerin79 <shinchaerin79@gmail.com>
2026-02-05 19:42:33 +0900

    :sparkles: Feat: 가게 태그 수정 Service 인터페이스 추가

--- src/main/java/com/umc9th/bizscan/domain/store/service/StoreService.java
+++ src/main/java/com/umc9th/bizscan/domain/store/service/StoreService.java
@@ -1,17 +1,22 @@
 package com.umc9th.bizscan.domain.store.service;
 
-import com.umc9th.bizscan.domain.store.dto.request.StoreRequest;
+import com.umc9th.bizscan.domain.store.dto.request.StoreCreateRequest;
+import com.umc9th.bizscan.domain.store.dto.request.StoreUpdateRequest;
 import com.umc9th.bizscan.domain.store.dto.response.StoreDeleteResponse;
 import com.umc9th.bizscan.domain.store.dto.response.StoreResponse;
 import java.util.List;
 
 public interface StoreService {
 
-  StoreResponse createStore(StoreRequest request);
+  StoreResponse createStore(String email, StoreCreateRequest request);
 
   List<StoreResponse> getStores();
 
   StoreResponse getStore(Long storeId);
+
+  StoreResponse updateStore(Long storeId, String email, StoreUpdateRequest request);
+
+  StoreResponse updateStoreTags(Long storeId, String email, List<String> tags);
 
   StoreDeleteResponse deleteStore(Long storeId);
 }
