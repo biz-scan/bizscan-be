2570bb73fa49025c7db8edc7c71f0a2d415be8f2
shinchaerin79 <shinchaerin79@gmail.com>
2026-02-09 14:04:48 +0900

    :bug: Fix: NoResourceFoundException 404 처리 import 수정

--- src/main/java/com/umc9th/bizscan/global/apiPayload/handler/GeneralExceptionAdvice.java
+++ src/main/java/com/umc9th/bizscan/global/apiPayload/handler/GeneralExceptionAdvice.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
-import org.springframework.web.reactive.resource.NoResourceFoundException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @RestControllerAdvice
 @RequiredArgsConstructor
