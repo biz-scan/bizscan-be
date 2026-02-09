f134ffd080759a232b2a11a024ed82f907a66e97
shinchaerin79 <shinchaerin79@gmail.com>
2026-02-09 20:39:11 +0900

    :sparkles: Feat: storeId 기준 최신 analysisId 조회 메서드 추가

--- src/main/java/com/umc9th/bizscan/domain/aiAnalysis/repository/AnalysisRepository.java
+++ src/main/java/com/umc9th/bizscan/domain/aiAnalysis/repository/AnalysisRepository.java
@@ -8,17 +8,27 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface AnalysisRepository extends JpaRepository<Analysis, Long> {
+
   Optional<Analysis> findByStoreId(long id);
 
   @Query(
       """
-  SELECT DISTINCT a FROM Analysis a
-  LEFT JOIN FETCH a.swots s
-  WHERE a.store.id = :storeId
-""")
+            SELECT DISTINCT a FROM Analysis a
+            LEFT JOIN FETCH a.swots s
+            WHERE a.store.id = :storeId
+          """)
   Optional<Analysis> findByStoreIdWithSwot(@Param("storeId") Long storeId);
 
   Optional<Analysis> findByStoreId(@Param("storeId") Long storeId);
 
   Optional<Analysis> findByStore(Store store);
+
+  @Query("""
+          select a.id
+          from Analysis a
+          where a.store.id = :storeId
+          order by a.createdAt desc
+      """)
+  Optional<Long> findLatestAnalysisIdByStoreId(@Param("storeId") Long storeId);
+
 }
