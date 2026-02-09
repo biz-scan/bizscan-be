e0c8e9f2561ce85132d0846dc0b695873479fb35
shinchaerin79 <shinchaerin79@gmail.com>
2026-02-09 14:11:01 +0900

    🐛 Fix: NoResourceFoundException 404 처리 import 수정
    * :bug: Fix: 루트(/) 요청 시 500 대신 404 응답하도록 예외 처리 보완
    
    * :bug: Fix: NoResourceFoundException 404 처리 import 수정
    
    * :wrench: Settings: Redis repo 스캔 경고 방지 위해 @EnableRedisRepositories 제거

--- src/main/java/com/umc9th/bizscan/global/security/config/RedisConfig.java
+++ src/main/java/com/umc9th/bizscan/global/security/config/RedisConfig.java
@@ -8,12 +8,10 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @RequiredArgsConstructor
-@EnableRedisRepositories
 @Configuration
 public class RedisConfig {
 
