91f963778bf6474e04af2124f17c90bcdef2dde8
kjh015 <khj843@naver.com>
2026-02-11 00:36:37 +0900

    ♻️ Refactor: 임시 permitAll

--- src/main/java/com/umc9th/bizscan/global/security/config/SecurityConfig.java
+++ src/main/java/com/umc9th/bizscan/global/security/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -32,8 +33,14 @@ public class SecurityConfig {
       JwtAccessDeniedHandler jwtAccessDeniedHandler)
       throws Exception {
     http.csrf(AbstractHttpConfigurer::disable)
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(
-            auth -> auth.requestMatchers(allowUris).permitAll().anyRequest().authenticated())
+            auth ->
+                auth
+                    // .requestMatchers(allowUris).permitAll().anyRequest().authenticated()
+                    .anyRequest()
+                    .permitAll())
         .formLogin(AbstractHttpConfigurer::disable)
         .exceptionHandling(
             handler ->
